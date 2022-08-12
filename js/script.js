cc65626a5ae9ba6506fda7756a94df17aff2b771
srdjan <srdjan.gr@gmail.com>
2022-08-12 17:53:10 +0200

    Dark/Light Theme option add with local storage. After page refresh stored theme will be checked and opened.

--- js/script.js
+++ js/script.js
@@ -13,10 +13,27 @@ function navToggle(){
 }
 
 
+// =====================================================
 // Theme Switch
 let slider = document.getElementById('slider');
+let bodyId = document.getElementById('bodyThemeSwitch');
 
 slider.addEventListener('click', () => {
     slider.classList.toggle('slider-move');
+    bodyId.classList.toggle('lightTheme');
+
+ const tema = document.querySelector('.lightTheme')
+    if(tema){
+        localStorage.setItem('lightTheme', 'enabled');
+    }else{
+        localStorage.setItem('lightTheme', null);
+    }
+
 })
+   
+let storedTheme = localStorage.getItem('lightTheme');
+
+if(storedTheme === 'enabled'){
+    document.body.classList.add('lightTheme');
+}
 
