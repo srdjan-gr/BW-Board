860549f08eb415309606fca5e17b5b364e379b60
srdjan <srdjan.gr@gmail.com>
2022-08-12 10:11:49 +0200

    Aside Menu 	- Open/Close menu functionality add 	- Open/Close transition add 	- CSS modified for aside linkks

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let menuSwitch = document.getElementById('menuSwitch');
 let asideMenu = document.getElementById('asideMenu');
 let switchIcon = document.getElementById('asideMenuIcon');
+let asideBoardsInfo = document.getElementById('asideBoardsInfo');
 
 menuSwitch.addEventListener('click', navToggle);
 
 function navToggle(){
     asideMenu.classList.toggle('closeAsideMenu');
     switchIcon.classList.toggle('icon-rotate');
+    asideBoardsInfo.classList.toggle('h2TextOff');
 }
